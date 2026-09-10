bd44b85db298a13a9dafe391c2bba34e59cab069
m1nk0der <maksimevsin3@gmail.com>
2026-09-10 21:08:03 +0500

    Task2

---
+++ Tyuiu.EvsinMA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.EvsinMA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.EvsinMA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Максим";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Максим", res);
+        }
+    }
+}
---
+++ Tyuiu.EvsinMA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.EvsinMA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.EvsinMA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Максим"));
+            Console.ReadKey();
+        }
+    }
+}
